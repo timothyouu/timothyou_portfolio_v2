8da22e364ec4820a7f0ce4ea5c9d7a5453585b2e
timothyouu <tlou@csu.fullerton.edu>
2025-06-13 17:16:54 -0700

    Added all the text and images needed for homepage, except for nav bar and footer.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,98 @@
 </script>
 
 <main>
-<footer></footer>
-  </main>
+<h1 class = "home-title group">Hello, I'm Timothy!!</h1>
+<h2 class = "home-subtitle group">Computer Science Student @CSUF</h2>
+<h3 class = "list group">List of Stuff I am</h3>
+<h4 class = "first fun-fact group">🤝 I'm an Open Source and Marketing Board member @acmcsuf</h4>
+<h4 class = "fun-fact group">🖥️ I play Valorant and Roblox in my free time</h4>
+<h4 class = "fun-fact group">📸 I love taking pictures</h4>
+<img src="/timothyou1.png" alt="Timothy Ou">
+</main>
 
 <style>
+
 :global(body) {
-  background-color: #0A081D;
+  background-color: #16142A;
   margin: 0;
   padding: 0;
 }
+.group {
+  margin-top: 175px;
+  margin-left: 200px;
+}
+
+.home-title {
+  color: #7D73EC;
+  -webkit-text-stroke-width: 1px;
+  -webkit-text-stroke-color: rgba(0, 0, 0, 0.00);
+  font-family: "Titan One";
+  font-size: 72px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+
+.home-subtitle {
+  width: 641px;
+  height: 40px;
+  margin-top: -50px;
+  flex-shrink: 0;
+  color: #BC95D1;
+  font-family: "Titan One";
+  font-size: 56px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+
+.list {
+  width: 374px;
+  height: 44px;
+  margin-top: 85px;
+  flex-shrink: 0;
+  color: #E0E1DD;
+  font-family: Kanit;
+  font-size: 48px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+}
+.fun-fact {
+  width: 850px;
+  height: 44px;
+  flex-shrink: 0;
+  color: #E0E1DD;
+  font-family: Kanit;
+  font-size: 48px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  margin-top: 75px;
+}
+
+.first {
+  margin-top: -30px;
+}
+
+img {
+  width: 798px;
+  height: 532px;
+  border: 10px solid #0A081D;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  width: 532px;
+  height: 532px;
+  border-radius: 50%;
+  overflow: hidden;
+  flex-shrink: 0;
+  background: url(/timothyou1.jpg) lightgray;
+  background-position: -116.252px -37.747px;
+  background-size: 166.98% 111.086%;
+  background-repeat: no-repeat;
+  margin-top: -590px;
+  margin-left: 1200px;
+  }
 </style>
