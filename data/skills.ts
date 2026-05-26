25a5240b234d347597b205c4c6a4dc314c6b6e4e
Timothy Ou <timothyou02@gmail.com>
2026-05-26 01:13:39 -0700

    feat: add typed data files for photos, projects, skills

---
+++ data/skills.ts
@@ -0,0 +1,11 @@
+export const programmingLanguages = ['C++', 'Javascript', 'Python', 'HTML', 'CSS', 'R']
+
+export const programmingTools = [
+  'Git', 'GitHub', 'Linux', 'Visual Studio Code',
+  'React Native Expo', 'Firebase', 'AWS S3', 'Crow', 'SvelteKit', 'RStudio',
+]
+
+export const otherSkills = [
+  'Problem Solving', 'Leadership', 'Teamwork', 'Communication',
+  'Collaboration', 'Adaptability', 'Mentoring', 'Marketing',
+]
